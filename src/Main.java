56c6cb019459d38efb9af4e8b2da3e9eeeaa43ad
memmedovnicatt <nicat.mammadov2022@khazar.org>
2024-06-24 19:31:18 +0400

    first

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        String s="ds";
+        int count=0;
+        System.out.println(count);
+
+    }
+}
